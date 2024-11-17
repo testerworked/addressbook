af03b3e9e0888e3866436cc15d92dc2ad72ea98a
Rinat Khuzin <khuzin.rinat.work@gmail.com>
2024-11-17 09:19:19 +0500

    code review;

--- app/src/main/java/com/sample/addressbook/MainActivity.kt
+++ app/src/main/java/com/sample/addressbook/MainActivity.kt
@@ -13,15 +13,15 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var firstNameInput: EditText
-    private lateinit var lastNameInput: EditText
+    private lateinit var fnameInput: EditText
+    private lateinit var lnameInput: EditText
     private lateinit var addressInput: EditText
     private lateinit var phoneInput: EditText
     private lateinit var saveButton: Button
-    private lateinit var listView: ListView
+    private lateinit var listViewAB: ListView
 
     private val personList = mutableListOf<Person>()
-    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var adapterAB: ArrayAdapter<String>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,29 +29,29 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        firstNameInput = findViewById(R.id.firstNameET)
-        lastNameInput = findViewById(R.id.lastNameET)
+        fnameInput = findViewById(R.id.firstNameET)
+        lnameInput = findViewById(R.id.lastNameET)
         addressInput = findViewById(R.id.addressET)
         phoneInput = findViewById(R.id.phoneET)
         saveButton = findViewById(R.id.saveB)
-        listView = findViewById(R.id.lvAB)
+        listViewAB = findViewById(R.id.lvAB)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf())
-        listView.adapter = adapter
+        adapterAB = ArrayAdapter(this, android.R.layout.simple_list_item_1, mutableListOf())
+        listViewAB.adapter = adapterAB
 
         saveButton.setOnClickListener {
-            val firstName = firstNameInput.text.toString()
-            val lastName = lastNameInput.text.toString()
+            val firstName = fnameInput.text.toString()
+            val lastName = lnameInput.text.toString()
             val address = addressInput.text.toString()
             val phone = phoneInput.text.toString()
 
             val person = Person(firstName, lastName, address, phone)
             personList.add(person)
-            adapter.add("$firstName $lastName")
+            adapterAB.add("$firstName $lastName")
             clearInputs()
         }
 
-        listView.setOnItemClickListener { _, _, position, _ ->
+        listViewAB.setOnItemClickListener { _, _, position, _ ->
             val person = personList[position]
             val intent = Intent(this, DetailActivity::class.java)
             intent.putExtra("person", person)
@@ -66,8 +66,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clearInputs() {
-        firstNameInput.text.clear()
-        lastNameInput.text.clear()
+        fnameInput.text.clear()
+        lnameInput.text.clear()
         addressInput.text.clear()
         phoneInput.text.clear()
     }
